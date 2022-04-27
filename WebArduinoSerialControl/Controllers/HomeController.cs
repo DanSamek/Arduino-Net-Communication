b4542d3c59db244005b0718891278b654a016604
Daniel Samek <samek@spstrutnov.cz>
2022-04-27 17:59:15 +0200

    Log action system + serial port fix

--- WebArduinoSerialControl/Controllers/HomeController.cs
+++ WebArduinoSerialControl/Controllers/HomeController.cs
@@ -11,12 +11,12 @@ namespace WebArduinoSerialControl.Controllers
 {
     public class HomeController : Controller
     {
-
+        string logsPath = System.IO.Path.GetFullPath(".") + @"\logs.txt";
         public IActionResult Index()
         {
             string[] ports = SerialPort.GetPortNames();
             string path = System.IO.Path.GetFullPath(".") + @"\options.csv";
-            if(!System.IO.File.Exists(path)) System.IO.File.Create(path);
+            if (!System.IO.File.Exists(path)) System.IO.File.Create(path);
             string[] csvData = System.IO.File.ReadAllLines(path);
             List<string> inputs = new List<string>();
             foreach (string option in csvData) {
@@ -29,43 +29,56 @@ namespace WebArduinoSerialControl.Controllers
 
         [HttpPost]
         public IActionResult Execute([FromBody] Data content)
-        {
+        {   
             content.execute = content.execute.Remove(content.execute.Length - 1);
             string[] separatedInput = content.execute.Split(";");
-            var response = "";
             string endString = "";
+            List<string> data = new List<string>();
+
             for (int index = 0; index < separatedInput.Length; index++) {
                 if (separatedInput.Length - 1 == index)
                     endString += separatedInput[index];
                 else endString += separatedInput[index] +";";
             }
+            string response = getResponse(endString, separatedInput);
+            data.Add(DateTime.Now.ToString() + ";" + content.execute + ";response-status: " + response);
+            System.IO.File.AppendAllLines(logsPath, data);
+            return Json(response);
+        }
+
+        public string getResponse(string endString, string[] separatedInput)
+        {
             SerialPort sp = new SerialPort();
-            Debug.WriteLine(endString);
-            try {
-                sp.PortName = separatedInput[0];
-                sp.BaudRate = 9600;
-                sp.Open();
-                sp.WriteLine(endString);
-                sp.Close();
-            }
-            catch (Exception ex) {
-                Console.WriteLine(ex.Message);
-            }
-            System.Threading.Thread.Sleep(2500);
             try
             {
                 sp.PortName = separatedInput[0];
                 sp.BaudRate = 9600;
-                sp.Open();                
-                response = sp.ReadLine();
-                sp.Close();
+                sp.Open();
+                sp.WriteLine(endString);
+                sp.DiscardInBuffer();
+                sp.DiscardOutBuffer();
+                System.Threading.Thread.Sleep(2000);
+                if(sp.BytesToRead > 0 ) { 
+                    var res = sp.ReadLine();
+                    sp.Close();
+                    return res;
+                }
             }
-            catch (Exception ex){
-                Console.WriteLine(ex.Message);
-            }
-            response = "";
-            return Json(response);
+            catch (Exception ex) { Debug.WriteLine(ex.Message); }
+            return "err";
         }
+        public IActionResult Logs() {
+
+            List<string> logs = new List<string>();
+            string[] data = System.IO.File.ReadAllLines(logsPath);
+            foreach (var dataitem in data)
+                logs.Add(dataitem);
+
+           
+            return View(logs);
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
